2cdf5b225b2168783936a8b56fd2efb607c0c955
Anton <antonzxcz@yahoo.dk>
2017-04-18 13:43:39 +0200

    added classes

---
+++ SWT-ATM/TrackSubject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT_ATM
+{
+    abstract class TrackSubject : ISubject
+    {
+
+        private List<int> list;
+
+        public void notify()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void attach()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deAttach()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

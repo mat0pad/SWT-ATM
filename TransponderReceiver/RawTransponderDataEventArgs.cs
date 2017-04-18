d67cd95dd5e4cd6c12982e542010446c83cd2c2e
Mathias <Mat_hias123@hotmail.com>
2017-04-18 11:13:45 +0200

    Init

---
+++ TransponderReceiver/RawTransponderDataEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TransponderReceiver
+{
+    public class RawTransponderDataEventArgs : EventArgs
+    {
+        public RawTransponderDataEventArgs(List<string> transponderData)
+        {
+            TransponderData = transponderData;
+        }
+        public List<string> TransponderData { get; }
+    }
+    public interface ITransponderReceiver
+    {
+        event EventHandler<RawTransponderDataEventArgs> TransponderDataReady;
+    }
+}

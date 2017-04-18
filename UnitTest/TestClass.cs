390abceb777e5143036ca19ffc3bed0ae3eca8da
Mathias <Mat_hias123@hotmail.com>
2017-04-18 11:43:05 +0200

    unittest setup

--- UnitTest/TestClass.cs
+++ UnitTest/TestClass.cs
@@ -13,7 +13,7 @@ namespace UnitTest
         [Test]
         public void TestMethod()
         {    
-            // TODO: Add your test code here teset gerg
+            // TODO: Add your test code here teset gergh
             Assert.Pass("Your first passing test");
         }
     }

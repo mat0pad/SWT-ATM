3acf14dade9b174be0a29ec2308b5c720693a3cb
Anton <antonzxcz@yahoo.dk>
2017-04-25 20:12:50 +0200

    added SubjectTest

---
+++ UnitTest/SubjectTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using SWT_ATM;
+
+namespace UnitTest
+{
+    [TestFixture]
+    class SubjectTest
+    {
+        private Subject<Data> subject;
+
+        [SetUp]
+        public void Init()
+        {
+            subject = new CoordinateMapper(null);
+        }
+
+        [Test]
+        public void NotifyObserverReceived()
+        {
+            SWT_ATM.IObserver<Data> observer = Substitute.For<SWT_ATM.IObserver<Data>>();
+            Data data = new Data("", 0, 0, 0, "");
+
+            subject.Attach(observer);
+
+            subject.Notify(data);
+
+            observer.Received(1).Update(data);
+        }
+
+        [Test]
+        public void NotifyObserverNotReceived()
+        {
+            SWT_ATM.IObserver<Data> observer = Substitute.For<SWT_ATM.IObserver<Data>>();
+            Data data = new Data("", 0, 0, 0, "");
+
+            subject.Attach(observer);
+
+            subject.Deattach(observer);
+
+            subject.Notify(data);
+
+            observer.DidNotReceive().Update(data);
+        }
+    }
+}
